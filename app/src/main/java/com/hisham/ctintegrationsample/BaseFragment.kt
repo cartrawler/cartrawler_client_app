4f13050f345795d9ffc91fac012e6567306a3411
Ian Arbuckle <ian.arbuckle18@gmail.com>
2020-09-07 16:39:50 +0100

    * Fix demo app bugs for app icon on settings screen and transitions

--- app/src/main/java/com/hisham/ctintegrationsample/BaseFragment.kt
+++ app/src/main/java/com/hisham/ctintegrationsample/BaseFragment.kt
@@ -1,11 +1,17 @@
 package com.hisham.ctintegrationsample
 
 import androidx.annotation.DrawableRes
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 open class BaseFragment : Fragment() {
 
     fun toolbarIcon(@DrawableRes drawableResId: Int) {
-        requireActivity().actionBar?.setLogo(drawableResId)
+        requireActivity().findViewById<Toolbar>(R.id.toolbar).logo = if (drawableResId != 0) {
+            ContextCompat.getDrawable(requireContext(), drawableResId)
+        } else {
+            null
+        }
     }
 }
